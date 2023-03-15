4309b100d75665d811d387d9e04a7979575cc084
Pavel Ermakov <petechka_petrov_201@mail.ru>
2023-03-15 23:57:17 +0300

    [lab2] refactor App

--- lab2/Viewer/App/App.cpp
+++ lab2/Viewer/App/App.cpp
@@ -5,13 +5,30 @@
 #include "App.h"
 #include "../MediaFrame/ImageFrame/ImageFrame.h"
 
+int MainLoop();
+bool RegisterWndClass(HINSTANCE hInstance);
+HWND CreateMainWindow(HINSTANCE hInstance);
 
-int App::Run(HINSTANCE hInstance, int nCmdShow)
+App::App(HINSTANCE hInstance, int nCmdShow)
+	: m_hInstance(hInstance)
+	, m_nCmdShow(nCmdShow)
 {
+}
+
+int App::Run()
+{
+	if (m_isRunning)
+	{
+		return 1;
+	}
+	m_isRunning = true;
+
 	try
 	{
+		GdiplusInitializer gdiplusInitializer;
+
 		// Регистрируем класс главного окна
-		if (!RegisterWndClass(hInstance))
+		if (!RegisterWndClass(m_hInstance))
 		{
 			return 1;
 		}
@@ -23,17 +40,19 @@ int App::Run(HINSTANCE hInstance, int nCmdShow)
 		AppendMenu(hMenu, MF_POPUP, (UINT_PTR)hSubMenu, L"File");
 
 		// Создаем главное окно приложения
-		HWND hMainWindow = CreateMainWindow(hInstance);
+		HWND hMainWindow = CreateMainWindow(m_hInstance);
 		if (hMainWindow == NULL)
 		{
 			return 1;
 		}
 
+		SetWindowLongPtr(hMainWindow, GWLP_USERDATA, (LONG_PTR)(this));
+
 		//Добавляем меню к главному окну приложения
 		SetMenu(hMainWindow, hMenu);
 
 		// Показываем главное окно приложения
-		ShowWindow(hMainWindow, nCmdShow);
+		ShowWindow(hMainWindow, m_nCmdShow);
 		UpdateWindow(hMainWindow);
 
 		// Запускаем цикл выборки сообщений, пока не получим
@@ -44,81 +63,6 @@ int App::Run(HINSTANCE hInstance, int nCmdShow)
 	{
 		return 2;
 	}
-}
-
-HWND App::CreateMainWindow(HINSTANCE hInstance)
-{
-	HWND hMainWindow = CreateWindowEx(
-		0,
-		WNDENTITIES.at(WndKeyEntities::CLASS_NAME),
-		WNDENTITIES.at(WndKeyEntities::WINDOW_TITLE),
-		WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, CW_USEDEFAULT,
-		CW_USEDEFAULT, CW_USEDEFAULT,
-		NULL,
-		NULL,
-		hInstance,
-		NULL);
-
-	return hMainWindow;
-}
-
-bool App::RegisterWndClass(HINSTANCE hInstance)
-{
-	WNDCLASSEX wndClass = {
-		sizeof(wndClass), // UINT cbSize;
-		CS_HREDRAW | CS_VREDRAW, // UINT style;
-		&WindowProc, // WNDPROC lpfnWndProc;
-		0, // int cbClsExtra;
-		0, // int cbWndExtra;
-		hInstance, // HINSTANCE hInstance;
-		NULL, // HICON hIcon;
-		LoadCursor(NULL, IDC_ARROW), // HCURSOR hCursor;
-		(HBRUSH)(COLOR_BTNFACE + 1), // HBRUSH hbrBackground;
-		NULL, // LPCTSTR lpszMenuName;
-		WNDENTITIES.at(WndKeyEntities::CLASS_NAME), // LPCTSTR lpszClassName;
-		NULL, // HICON hIconSm;
-	};
-
-	return RegisterClassEx(&wndClass) != FALSE;
-}
-
-int App::MainLoop()
-{
-	MSG msg;
-	BOOL res;
-	while ((res = GetMessage(&msg, NULL, 0, 0)) != 0)
-	{
-		if (res == -1)
-		{
-			// произошла ошибка - нужно обработать ее и, вероятно,
-			// завершить работу приложения
-		}
-		else
-		{
-			// Если это сообщение о нажатии виртуальной клавиши,
-			// то добавляем в очередь сообщений сообщения, несущие информацию о
-			// коде вводимого пользователем символа
-			TranslateMessage(&msg);
-			// передаем сообщение в соответствующую оконную процедуру
-			DispatchMessage(&msg);
-		}
-	}
-
-	// сюда мы попадем только в том случае извлечения сообщения WM_QUIT
-	// msg.wParam содержит код возврата, помещенный при помощи функции PostQuitMessage()
-	return msg.wParam;
-}
-
-LRESULT App::WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
-{
-	switch (uMsg)
-	{
-		HANDLE_MSG(hwnd, WM_DESTROY, OnDestroy);
-		HANDLE_MSG(hwnd, WM_PAINT, OnPaint);
-		HANDLE_MSG(hwnd, WM_COMMAND, OnCommand);
-	}
-	return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
 
 void App::InitFileNameStructure(HWND hwndOwner, OPENFILENAME* pOpenFileName, TCHAR* pFileName, DWORD maxFileName)
@@ -156,13 +100,116 @@ void App::OnOpenFile(HWND hwnd, UINT)
 
 		if (img.GetLastStatus() == Gdiplus::Ok)
 		{
-			auto bitmap = new Gdiplus::Bitmap(img.GetWidth(), img.GetHeight(), PixelFormat32bppARGB);
-			Gdiplus::Graphics g(bitmap);
+			auto bitmap = std::make_unique<Gdiplus::Bitmap>(img.GetWidth(), img.GetHeight(), PixelFormat32bppARGB);
+			Gdiplus::Graphics g(bitmap.get());
 			g.DrawImage(&img, 0, 0);
-			m_mediaFrames.push_back(std::make_unique<ImageFrame>(bitmap));
+			//TODO: исправить мув на муве
+			m_mediaFrames.push_front(std::move(std::make_unique<ImageFrame>(std::move(bitmap))));
 
 			// триггерим событие перерисовки
 			InvalidateRect(hwnd, NULL, TRUE);
 		}
 	}
+}
+
+void App::OnPaint(HWND hwnd)
+{
+	PAINTSTRUCT ps;
+	HDC dc = BeginPaint(hwnd, &ps);
+
+	Gdiplus::Graphics g(dc);
+
+
+
+	for (auto&& mediaFrame : m_mediaFrames)
+	{
+		auto resizeCoef = mediaFrame->WndFit(hwnd);
+		mediaFrame->Center(hwnd, resizeCoef);
+		mediaFrame->Display(g);
+	}
+}
+
+void App::OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify)
+{
+	if (id == MenuID.at(MenuIDKey::NEW))
+	{
+		OnOpenFile(hwnd, codeNotify);
+	}
+}
+
+LRESULT WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	auto instance = reinterpret_cast<App*>(GetWindowLongPtr(hwnd, GWLP_USERDATA));
+
+	switch (uMsg)
+	{
+		HANDLE_MSG(hwnd, WM_DESTROY, instance->OnDestroy);
+		HANDLE_MSG(hwnd, WM_PAINT, instance->OnPaint);
+		HANDLE_MSG(hwnd, WM_COMMAND, instance->OnCommand);
+	}
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+HWND CreateMainWindow(HINSTANCE hInstance)
+{
+	HWND hMainWindow = CreateWindowEx(
+		0,
+		WNDENTITIES.at(WndKeyEntities::CLASS_NAME),
+		WNDENTITIES.at(WndKeyEntities::WINDOW_TITLE),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+
+	return hMainWindow;
+}
+
+bool RegisterWndClass(HINSTANCE hInstance)
+{
+	WNDCLASSEX wndClass = {
+		sizeof(wndClass), // UINT cbSize;
+		CS_HREDRAW | CS_VREDRAW, // UINT style;
+		&WindowProc, // WNDPROC lpfnWndProc;
+		0, // int cbClsExtra;
+		static_cast<LONG_PTR>(sizeof(App*)), // int cbWndExtra;
+		hInstance, // HINSTANCE hInstance;
+		NULL, // HICON hIcon;
+		LoadCursor(NULL, IDC_ARROW), // HCURSOR hCursor;
+		(HBRUSH)(COLOR_BTNFACE + 1), // HBRUSH hbrBackground;
+		NULL, // LPCTSTR lpszMenuName;
+		WNDENTITIES.at(WndKeyEntities::CLASS_NAME), // LPCTSTR lpszClassName;
+		NULL, // HICON hIconSm;
+	};
+
+	return RegisterClassEx(&wndClass) != FALSE;
+}
+
+int MainLoop()
+{
+	MSG msg;
+	BOOL res;
+	while ((res = GetMessage(&msg, NULL, 0, 0)) != 0)
+	{
+		if (res == -1)
+		{
+			// произошла ошибка - нужно обработать ее и, вероятно,
+			// завершить работу приложения
+		}
+		else
+		{
+			// Если это сообщение о нажатии виртуальной клавиши,
+			// то добавляем в очередь сообщений сообщения, несущие информацию о
+			// коде вводимого пользователем символа
+			TranslateMessage(&msg);
+			// передаем сообщение в соответствующую оконную процедуру
+			DispatchMessage(&msg);
+		}
+	}
+
+	// сюда мы попадем только в том случае извлечения сообщения WM_QUIT
+	// msg.wParam содержит код возврата, помещенный при помощи функции PostQuitMessage()
+	return static_cast<int>(msg.wParam);
 }
--- lab2/Viewer/App/App.h
+++ lab2/Viewer/App/App.h
@@ -1,26 +1,29 @@
 #pragma once
+#include <deque>
+#include <memory>
 #include "../MediaFrame/IMediaFrame.h"
 
 class App
 {
 public:
-	int Run(HINSTANCE hInstance, int nCmdShow);
+	App(HINSTANCE hInstance, int nCmdShow = SW_SHOW);
 
-private:
-	HWND CreateMainWindow(HINSTANCE hInstance);
-	bool RegisterWndClass(HINSTANCE hInstance);
-	int MainLoop();
-	LRESULT CALLBACK WindowProc(
-		HWND hwnd,
-		UINT uMsg,
-		WPARAM wParam,
-		LPARAM lParam);
-
-	void InitFileNameStructure(HWND hwndOwner, OPENFILENAME* pOpenFileName, TCHAR* pFileName, DWORD maxFileName);
+	int Run();
 
 	void OnDestroy(HWND /*hWnd*/);
 	void OnOpenFile(HWND hwnd, UINT /*codeNotify*/);
+	void OnPaint(HWND hwnd);
+	void OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify);
 
+
+private:
+	void InitFileNameStructure(HWND hwndOwner, OPENFILENAME* pOpenFileName, TCHAR* pFileName, DWORD maxFileName);
+
+	//LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+	bool m_isRunning = false;
 	HINSTANCE m_hInstance;
-	std::vector<std::unique_ptr<IMediaFrame>> m_mediaFrames;
+	int m_nCmdShow;
+	std::deque<std::unique_ptr<IMediaFrame>> m_mediaFrames;
+	UINT m_mediaFrameIndex;
 };
