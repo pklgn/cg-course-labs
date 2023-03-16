50ba63672cb3c76a4ece96f5576413e1679aa27c
Pavel Ermakov <petechka_petrov_201@mail.ru>
2023-03-16 03:02:35 +0300

    [lab2] refactor

--- lab2/Viewer/MediaFrame/IMediaFrame.h
+++ lab2/Viewer/MediaFrame/IMediaFrame.h
@@ -1,12 +1,15 @@
 #pragma once
 #include <windows.h>
+#include <gdiplus.h>
 
 class IMediaFrame
 {
 public:
 	virtual ~IMediaFrame() = default;
 
-	virtual void Display(HWND hwnd) = 0;
+	virtual void Display(Gdiplus::Graphics& g) = 0;
 	virtual void Resize(SIZE size) = 0;
 	virtual void Move(POINT dst) = 0;
+	virtual void Center(HWND hwnd, double resizeCoef = 1) = 0;
+	virtual double WndFit(HWND hwnd) = 0;
 };
