2e90af5914aa991c11c1232da9060cf47fffb416
Pavel Ermakov <petechka_petrov_201@mail.ru>
2023-03-27 21:17:51 +0300

    [lab2] set ImageFrameView as observer

--- lab2/Viewer_MFC/View/ImageFrameView/ImageFrameView.cpp
+++ lab2/Viewer_MFC/View/ImageFrameView/ImageFrameView.cpp
@@ -1,2 +1,31 @@
-#include "../../pch.h"
+﻿#include "../../pch.h"
 #include "ImageFrameView.h"
+
+
+ImageFrameView::ImageFrameView(ImageFrame& imageFrame)
+	: m_imageFrame(imageFrame)
+{
+	imageFrame.AddObserver(*this);
+}
+
+ImageFrameView::~ImageFrameView()
+{
+	m_imageFrame.RemoveObserver(*this);
+	// TODO: подумать над тем, как правильно удалять миниатюру битмапы
+	delete *m_pThumbnailImage.get();
+}
+
+void ImageFrameView::Display(Gdiplus::Graphics& g)
+{
+	auto leftTop = m_imageFrame.GetLeftTop();
+	g.DrawImage(*m_pThumbnailImage.get(), static_cast<INT>(leftTop.x), static_cast<INT>(leftTop.y));
+}
+
+void ImageFrameView::Update(const ImageFrameData& data)
+{
+	if (data.pBitmap != std::nullopt ||
+		data.size != std::nullopt)
+	{
+		m_pThumbnailImage = std::make_unique<Gdiplus::Image*>(m_imageFrame.GetThumbnailImage(data.size.value()));
+	}
+}
