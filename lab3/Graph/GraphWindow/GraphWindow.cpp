4377db576183657815f0540c7ce9f77b0553302d
Pavel Ermakov <petechka_petrov_201@mail.ru>
2023-04-10 16:46:34 +0300

    [CHERNO-8] add shader reader and separate shader file

--- lab3/Graph/GraphWindow/GraphWindow.cpp
+++ lab3/Graph/GraphWindow/GraphWindow.cpp
@@ -1,9 +1,11 @@
 ﻿#define _USE_MATH_DEFINES
-#include <cmath>
 #include "GraphWindow.h"
+#include <cmath>
+#include <fstream>
+#include <sstream>
+#include <string>
 
 #pragma comment(lib, "glew32.lib")
-
 
 constexpr GLfloat X_MIN = -1;
 constexpr GLfloat X_MAX = 1;
@@ -16,7 +18,57 @@ constexpr GLfloat MIN_X_RANGE_BOUND = -6 * M_PI;
 constexpr GLfloat MAX_X_RANGE_BOUND = 6 * M_PI;
 constexpr float X_STEP = 0.0001f;
 
-// NEW
+struct ShaderProgramSource
+{
+	std::string vertexShader;
+	std::string framgentShader;
+};
+
+static ShaderProgramSource ParseShader(const std::string& filepath)
+{
+	std::ifstream stream(filepath);
+
+	enum class ShaderType
+	{
+		NONE = -1,
+		VERTEX = 0,
+		FRAGMENT = 1,
+	};
+
+	if (!stream)
+	{
+		std::cout << "Unable to open filestream at " << filepath << std::endl;
+		throw std::invalid_argument("Invalid filepath");
+	}
+
+	std::string line;
+	std::stringstream ss[2];
+	ShaderType shaderType = ShaderType::NONE;
+	while (std::getline(stream, line))
+	{
+		if (line.find("#shader") != std::string::npos)
+		{
+			if (line.find("vertex") != std::string::npos)
+			{
+				shaderType = ShaderType::VERTEX;
+			}
+			else if (line.find("fragment") != std::string::npos)
+			{
+				shaderType = ShaderType::FRAGMENT;
+			}
+		}
+		else
+		{
+			ss[(int)shaderType] << line << "\n";
+		}
+	}
+
+	return {
+		ss[0].str(),
+		ss[1].str()
+	};
+}
+
 static unsigned int CompileShader(unsigned int type, const std::string& source)
 {
 	unsigned int id = glCreateShader(type);
@@ -42,7 +94,6 @@ static unsigned int CompileShader(unsigned int type, const std::string& source)
 	return id;
 }
 
-//NEW
 static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
 {
 	unsigned int program = glCreateProgram();
@@ -117,8 +168,8 @@ void GraphWindow::Draw(int width, int height)
 
 	float positions[6] = {
 		-0.5f, -0.5f,
-		 0.5f, -0.5f,
-		 0.0f,  0.5f
+		0.5f, -0.5f,
+		0.0f, 0.5f
 	};
 	unsigned int buffer;
 	glGenBuffers(1, &buffer);
@@ -130,7 +181,7 @@ void GraphWindow::Draw(int width, int height)
 	// type = GL_FLOAT, потому что флоат)
 	// normalize = FALSE, вроде как нормализовать не надо, все норм
 	// stride = 8, это количество байт, которое мы имеем между каждой вершиной, это величина именно до следующей ВЕРШИНЫ (vertex), НЕ АТРБИБУТА (attribute)
-	// pointer = 0 помним, что указатель это тоже число, которое мы передаем для КОНКРЕТНОЙ ВЕРШИНЫ и в рамках этой вершины прибавляем к ее стартовому адресу 
+	// pointer = 0 помним, что указатель это тоже число, которое мы передаем для КОНКРЕТНОЙ ВЕРШИНЫ и в рамках этой вершины прибавляем к ее стартовому адресу
 	// указатель, чтобы получить доступ к конкретному атрибуту, можно использовать offsetof макрос, если у нас уже определена конкретная структура в качестве текущего атрибута
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(positions[0]) * 2, 0);
 	// MEM: нужно заэнейблить этот атрибут
@@ -138,47 +189,31 @@ void GraphWindow::Draw(int width, int height)
 
 	// есть два основных типов шейдеров: vertex-шейдер и fragment-шейдер (также известных как pixel-шейдеры)
 	/*
-	* Получаем следующую последовательность действий
-	* 1. Draw call
-	* 2. Vertex shader
-	* 3. Fragment shader
-	* 4. Получаем возможность увидеть на экране, что ожидали
-	* 
-	* Получается, что вертекс шейдер вызывается отдельно для каждой вершины, поскольку мы рисуем треугольник, у которого три вершины, то 
-	* это означает, что вертекс шейдер будет вызван трижды
-	* Основная цель вертекс шейдера в том, чтобы сказать ОПЕНЖЫЭЛ где мы хотим видеть нашу вершину на нашем скрин спейс
-	* Шейдер не связан со светом и тенью, это программа
-	* Все, что делает вертекс шейдер - определяет какой мы хотим видеть позицию, потому что как мы уже знаем, в ОПЕНЖЫЭЛе все координаты нормированы
-	* и их надо привести к реальным
-	*
-	* После вертекс этапа наступает очередь фрагмент-шейдеров
-	* Будем для простоты пока считать, что фрагменты это пиксели
-	* Каждый фрагмент шейдер будет выполнен единожды для каждого пикселя, который требует растеризации
-	* Фрагмент шейдер решает, каким цветом должен быть каждый пиксель
-	* Прикольное сравнение с раскраской, сначала определили вершины, а затем закрасили в установленных границах нужным цветом
-	* Опять ниточка со стейт машиной, которая нам напоминает, что нужно ЗАЭНЕЙБЛИТЬ шейдер, чтобы его использовать
-	*/
-
-
-	// NEW
-	std::string vertexShader = "#version 330 core\n"
-							   "\n"
-							   "layout(location = 0) in vec4 position;\n"
-							   "void main()\n"
-							   "{\n"
-							   "	gl_Position = position;\n"
-							   "}\n";
+	 * Получаем следующую последовательность действий
+	 * 1. Draw call
+	 * 2. Vertex shader
+	 * 3. Fragment shader
+	 * 4. Получаем возможность увидеть на экране, что ожидали
+	 *
+	 * Получается, что вертекс шейдер вызывается отдельно для каждой вершины, поскольку мы рисуем треугольник, у которого три вершины, то
+	 * это означает, что вертекс шейдер будет вызван трижды
+	 * Основная цель вертекс шейдера в том, чтобы сказать ОПЕНЖЫЭЛ где мы хотим видеть нашу вершину на нашем скрин спейс
+	 * Шейдер не связан со светом и тенью, это программа
+	 * Все, что делает вертекс шейдер - определяет какой мы хотим видеть позицию, потому что как мы уже знаем, в ОПЕНЖЫЭЛе все координаты нормированы
+	 * и их надо привести к реальным
+	 *
+	 * После вертекс этапа наступает очередь фрагмент-шейдеров
+	 * Будем для простоты пока считать, что фрагменты это пиксели
+	 * Каждый фрагмент шейдер будет выполнен единожды для каждого пикселя, который требует растеризации
+	 * Фрагмент шейдер решает, каким цветом должен быть каждый пиксель
+	 * Прикольное сравнение с раскраской, сначала определили вершины, а затем закрасили в установленных границах нужным цветом
+	 * Опять ниточка со стейт машиной, которая нам напоминает, что нужно ЗАЭНЕЙБЛИТЬ шейдер, чтобы его использовать
+	 */
 
 	// NEW
-	std::string fragmentShader = "#version 330 core\n"
-								 "\n"
-								 "layout(location = 0) out vec4 color;\n"
-								 "void main()\n"
-								 "{\n"
-								 "	color = vec4(1.0, 0.0, 0.0, 1.0);\n"
-								 "}\n";
+	ShaderProgramSource shaderProgramSource = ParseShader("Resources/Shaders/Shader.shader");
 
-	auto shader = CreateShader(vertexShader, fragmentShader);
+	auto shader = CreateShader(shaderProgramSource.vertexShader, shaderProgramSource.framgentShader);
 	glUseProgram(shader);
 
 	glDrawArrays(GL_TRIANGLES, 0, 3);
