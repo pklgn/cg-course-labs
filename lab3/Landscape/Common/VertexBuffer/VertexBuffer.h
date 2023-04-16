ac1b4b6809928ac40f617f6de473b2a32787983f
Pavel Ermakov <petechka_petrov_201@mail.ru>
2023-04-16 23:56:29 +0300

    [lab3] add VertexBuffer basics

---
+++ lab3/Landscape/Common/VertexBuffer/VertexBuffer.h
@@ -0,0 +1,69 @@
+/*
+	Copyright (C) 2012 Alexander Overvoorde
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy of
+	this software and associated documentation files (the "Software"), to deal in
+	the Software without restriction, including without limitation the rights to
+	use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+	the Software, and to permit persons to whom the Software is furnished to do so,
+	subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+	FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+	COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+	IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+	CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE
+*/
+
+#pragma once
+
+#include <GL/glew.h>
+#include <cstdint>
+#include <functional>
+
+/*
+	Buffer usage types
+*/
+namespace BufferUsage
+{
+enum buffer_usage_t
+{
+	StreamDraw = GL_STREAM_DRAW,
+	StreamRead = GL_STREAM_READ,
+	StreamCopy = GL_STREAM_COPY,
+	StaticDraw = GL_STATIC_DRAW,
+	StaticRead = GL_STATIC_READ,
+	StaticCopy = GL_STATIC_COPY,
+	DynamicDraw = GL_DYNAMIC_DRAW,
+	DynamicRead = GL_DYNAMIC_READ,
+	DynamicCopy = GL_DYNAMIC_COPY
+};
+}
+
+/*
+	Vertex Buffer
+*/
+class VertexBuffer
+{
+public:
+	VertexBuffer();
+	VertexBuffer(const VertexBuffer& other);
+	VertexBuffer(const void* data, size_t length, BufferUsage::buffer_usage_t usage);
+
+	~VertexBuffer();
+
+	operator GLuint() const;
+	const VertexBuffer& operator=(const VertexBuffer& other);
+
+	void Data(const void* data, size_t length, BufferUsage::buffer_usage_t usage);
+	void SubData(const void* data, size_t offset, size_t length);
+
+	void GetSubData(void* data, size_t offset, size_t length);
+
+private:
+	GLuint obj;
+};
