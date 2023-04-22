5bc1d2b0cc4a606ee69e0ce51b29123428b6e95d
Pavel Ermakov <petechka_petrov_201@mail.ru>
2023-04-22 19:46:25 +0300

    [deps] introduce Glance library

---
+++ Glance/Glance/Common/OpenGLPrimitive/BasePrimitive.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <GL/glew.h>
+#include <vector>
+#include "../../Common/Types/DimensionTypes.h"
+#include "../../Common/Types/ColorTypes.h"
+
+class BasePrimitive
+{
+public:
+	static const RGB DEFAULT_COLOR;
+	BasePrimitive(Size size, Vector3d position, float angle = 0.f);
+	virtual ~BasePrimitive();
+
+	// TODO: remove vertices methods?
+	void SetVerticesData(const std::vector<GLfloat>& vertices);
+	std::vector<GLfloat> GetVerticesData() const;
+
+	void SetSize(Size size);
+	Size GetSize() const;
+
+	void SetPosition(Vector3d position);
+	Vector3d GetPosition() const;
+
+	void SetAngle(float angle);
+	float GetAngle() const;
+
+	virtual void Draw(GLuint program) const = 0;
+
+protected:
+	void UpdateData();
+	void ApplyModelTransform(GLuint program) const;
+	std::vector<RGB> GetVerticesColor(const std::vector<RGB>& colors, unsigned int verticesNumber) const;
+
+	// TODO: сделать отдельные обертки и сделать их вью онли
+	GLuint m_vao;
+	GLuint m_vbo;
+	Size m_size;
+	float m_angle;
+	Vector3d m_position;
+	std::vector<GLfloat> m_verticesData;
+};
---
+++ Glance/Glance/Common/OpenGLPrimitive/Curves/QuadraticBezier/QuadraticBezier.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "../../../Common/Types/GeometryTypes.h"
+#include "../../BasePrimitive.h"
+#include <GL/glew.h>
+
+class QuadraticBezier : public BasePrimitive
+{
+public:
+	QuadraticBezier(Size size, Vector3d position, const Curve4d& controlPoints, const std::vector<RGB>& colors = {}, float angle = 0.f, unsigned int verticesNumber = 100);
+
+	void Draw(GLuint program) const override;
+
+	void SetDrawMode(GLenum mode);
+
+private:
+	unsigned int m_verticesNumber;
+	Curve4d m_controlPoints;
+	GLenum m_drawMode;
+};
